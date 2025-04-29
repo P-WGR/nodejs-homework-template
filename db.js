1727dbfc7b931b251268300db806a0efb5fc8003
Piotr Wagner <wgr.animations@gmail.com>
2025-04-29 21:45:55 +0200

    fix db.js

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const uri = process.env.MONGO_URL;
-
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Database connection successful'))
-  .catch((error) => {
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connection successful');
+  } catch (error) {
     console.error('Database connection error:', error.message);
     process.exit(1);
-  });
+  }
+};
+
+connectDb();
