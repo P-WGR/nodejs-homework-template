90387925a619edd389d946d84d98c1dc35008a87
Piotr Wagner <wgr.animations@gmail.com>
2025-05-07 17:25:52 +0200

    Small update from hw03

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const connectDB = require('./db');
+connectDB();
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const contactsRouter = require('./routes/api/contacts');
+app.use('/api/contacts', contactsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Contacts API!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
+
+module.exports = app;
