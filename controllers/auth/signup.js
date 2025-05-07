31d535768452772ed7e6ba03645a2e494481121f
Piotr Wagner <wgr.animations@gmail.com>
2025-05-07 18:19:11 +0200

    update app.js and added: signup / auth

---
+++ controllers/auth/signup.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcryptjs");
+const Joi = require("joi");
+
+const User = require("../../models/user");
+
+const signupSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const signup = async (req, res, next) => {
+  try {
+    const { error } = signupSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email in use" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      user: {
+        email: newUser.email,
+        subscription: newUser.subscription,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = signup;
