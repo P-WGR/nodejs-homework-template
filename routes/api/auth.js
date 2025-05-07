6e254fd7c342f8b063b666641430f362ee737597
Piotr Wagner <wgr.animations@gmail.com>
2025-05-07 18:40:25 +0200

    Update step 5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const signup = require("../../controllers/auth/signup");
 const login = require("../../controllers/auth/login");
+const getCurrentUser = require("../../controllers/auth/getCurrentUser");
+
+const auth = require("../../middlewares/auth");
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/current", auth, getCurrentUser);
 
 module.exports = router;
